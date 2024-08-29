f45f0cf1267387e8a4ca64ff60975d1db99549e6
GalacticKittenSS <daniel05james@gmail.com>
2024-08-29 13:05:31 +0100

    AWS FileUtils.py now uses an environment variable for bucket

--- src/aws/FileUtils.py
+++ src/aws/FileUtils.py
@@ -4,20 +4,20 @@ import s3fs
 # AWS Access Token
 aws_id = os.environ['AWS_ID']
 aws_secret = os.environ['AWS_SECRET']
-bucket = 'galactickittenss'
+bucket = os.environ['AWS_BUCKET']
 s3 = s3fs.S3FileSystem(key=aws_id, secret=aws_secret)
 
 def ListDirectory(directory : str) -> list[str]:
-    directory = f'{bucket}/blog/{directory}/'
+    directory = f'{bucket}/{directory}/'
     objs : list[str] = s3.listdir(directory, detail=False)
     return [file.replace(directory, '') for file in objs if file != directory]
 
 def Exists(filename : str) -> bool:
-    return s3.exists(f'{bucket}/blog/{filename}')
+    return s3.exists(f'{bucket}/{filename}')
 
 def ReadBytes(filename : str) -> bytes:
-    with s3.open(f'galactickittenss/blog/{filename}', 'rb') as f:
-        return f.read()
+    with s3.open(f'{bucket}/{filename}', 'rb') as f:
+        return f.read() # type: ignore
 
 def Read(filename : str) -> str:
     return ReadBytes(filename).decode()
