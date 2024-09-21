f5c1b51c923ad86cc5c618768269d7516b6d0596
GalacticKittenSS <daniel05james@gmail.com>
2024-09-21 12:25:32 +0100

    Added GetFilesRecursively to AWS
    - Fixed ListDirectory returning folders

--- bards/aws/FileUtils.py
+++ bards/aws/FileUtils.py
@@ -9,13 +9,19 @@ s3 = s3fs.S3FileSystem(key=aws_id, secret=aws_secret)
 
 def ListDirectory(directory : str) -> list[str]:
     directory = f'{bucket}/{directory}/'
-    objs : list[str] = s3.listdir(directory, detail=False)
-    return [file.replace(directory, '') for file in objs if file != directory]
+    objs : list[dict[str, str]] = s3.listdir(directory, detail=True)
+    return [file['Key'].replace(directory, '') for file in objs if file['type'] == 'file']
+
+def GetFilesRecursively(directory : str) -> list[str]:
+    directory = f'{bucket}/{directory}/'
+    objs = s3.find(directory, detail=False)
+    return [file.replace(directory, '') for file in objs]
 
 def Exists(filename : str) -> bool:
     return s3.exists(f'{bucket}/{filename}')
 
 def ReadBytes(filename : str) -> bytes:
+    print(f"Opening '{bucket}/{filename}'")
     with s3.open(f'{bucket}/{filename}', 'rb') as f:
         return f.read() # type: ignore
 
